ff36f36479af2f7ea5dcae68432477d07c917945
Neko Aria <990879119@qq.com>
2024-03-21 16:38:09 +0800

    feat(filter): 搜索忽略大小写

--- src/utils/Filter.js
+++ src/utils/Filter.js
@@ -1,22 +1,25 @@
-export const filterEntries = (
-  entries,
-  filterType,
-  filterStatus,
-  filterString,
-) => {
+const includesIgnoreCase = (str, searchValue) => {
+  return str.toLowerCase().includes(searchValue.toLowerCase());
+};
+
+const filterEntries = (entries, filterType, filterStatus, filterString) => {
   // 0: title 1: content
   if (filterType === "0") {
     return filterStatus === "all"
-      ? entries.filter((entry) => entry.title.includes(filterString))
+      ? entries.filter((entry) => includesIgnoreCase(entry.title, filterString))
       : entries.filter(
           (entry) =>
-            entry.title.includes(filterString) && entry.status === filterStatus,
+            includesIgnoreCase(entry.title, filterString) &&
+            entry.status === filterStatus,
         );
   }
   return filterStatus === "all"
-    ? entries.filter((entry) => entry.content.includes(filterString))
+    ? entries.filter((entry) => includesIgnoreCase(entry.content, filterString))
     : entries.filter(
         (entry) =>
-          entry.content.includes(filterString) && entry.status === filterStatus,
+          includesIgnoreCase(entry.content, filterString) &&
+          entry.status === filterStatus,
       );
 };
+
+export { includesIgnoreCase, filterEntries };
