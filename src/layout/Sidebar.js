fe85f35dc343351dada7376aa9fec0eb1c5fb1a6
electh <noteperson@gmail.com>
2024-03-23 15:45:58 +0800

    侧边栏与未读筛选联动

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -17,13 +17,23 @@ function MenuTitle({ title, unread }) {
         alignItems: "center",
       }}
     >
-      <Typography.Ellipsis expandable={false} style={{ flex: 1 }}>
+      <Typography.Ellipsis
+        expandable={false}
+        style={{ width: unread > 0 ? "75%" : "100%" }}
+      >
         {title}
       </Typography.Ellipsis>
       {unread > 0 ? (
-        <Typography.Text style={{ color: "var(--color-text-4)" }}>
+        <Typography.Ellipsis
+          style={{
+            color: "var(--color-text-4)",
+            width: "25%",
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
+        >
           {unread}
-        </Typography.Text>
+        </Typography.Ellipsis>
       ) : null}
     </div>
   );
@@ -36,6 +46,7 @@ export default function Sidebar({ style, setVisible }) {
   const categories = useStore((state) => state.categories);
   const loading = useStore((state) => state.loading);
   const isMobile = useStore((state) => state.isMobile);
+  const unreadOnly = useStore((state) => state.unreadOnly);
 
   const [params] = useSearchParams();
   const from = params.get("from") || "all";
@@ -101,26 +112,37 @@ export default function Sidebar({ style, setVisible }) {
             </span>
           }
         >
-          {categories.map((category) => (
-            <Menu.Item
-              key={`category${category.id}`}
-              onClick={() => {
-                isMobile && setVisible(false);
-                nav(`/?from=category&id=${category.id}`);
-              }}
-            >
-              <MenuTitle
-                title={category.title}
-                unread={
-                  entries.filter(
-                    (entry) =>
-                      entry.feed.category.id === category.id &&
-                      entry.status === "unread",
-                  ).length
-                }
-              />
-            </Menu.Item>
-          ))}
+          {categories
+            .filter(
+              (category) =>
+                entries.filter(
+                  (entry) =>
+                    entry.feed.category.id === category.id &&
+                    (unreadOnly
+                      ? entry.status === "unread"
+                      : entry.status.length > 0),
+                ).length > 0,
+            )
+            .map((category) => (
+              <Menu.Item
+                key={`category${category.id}`}
+                onClick={() => {
+                  isMobile && setVisible(false);
+                  nav(`/?from=category&id=${category.id}`);
+                }}
+              >
+                <MenuTitle
+                  title={category.title}
+                  unread={
+                    entries.filter(
+                      (entry) =>
+                        entry.feed.category.id === category.id &&
+                        entry.status === "unread",
+                    ).length
+                  }
+                />
+              </Menu.Item>
+            ))}
         </Menu.SubMenu>
         <Menu.SubMenu
           key="feed"
@@ -131,25 +153,36 @@ export default function Sidebar({ style, setVisible }) {
             </span>
           }
         >
-          {feeds.map((feed) => (
-            <Menu.Item
-              key={`feed${feed.id}`}
-              onClick={() => {
-                isMobile && setVisible(false);
-                nav(`/?from=feed&id=${feed.id}`);
-              }}
-            >
-              <MenuTitle
-                title={feed.title}
-                unread={
-                  entries.filter(
-                    (entry) =>
-                      entry.feed.id === feed.id && entry.status === "unread",
-                  ).length
-                }
-              />
-            </Menu.Item>
-          ))}
+          {feeds
+            .filter(
+              (feed) =>
+                entries.filter(
+                  (entry) =>
+                    entry.feed.id === feed.id &&
+                    (unreadOnly
+                      ? entry.status === "unread"
+                      : entry.status.length > 0),
+                ).length > 0,
+            )
+            .map((feed) => (
+              <Menu.Item
+                key={`feed${feed.id}`}
+                onClick={() => {
+                  isMobile && setVisible(false);
+                  nav(`/?from=feed&id=${feed.id}`);
+                }}
+              >
+                <MenuTitle
+                  title={feed.title}
+                  unread={
+                    entries.filter(
+                      (entry) =>
+                        entry.feed.id === feed.id && entry.status === "unread",
+                    ).length
+                  }
+                />
+              </Menu.Item>
+            ))}
         </Menu.SubMenu>
       </Menu>
     </Skeleton>
