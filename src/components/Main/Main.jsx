c4178be168dc0f98e091c0bd858300aac358ef8c
Neko Aria <990879119@qq.com>
2024-03-24 20:38:25 +0800

    refactor(component): 将一些子组件从父组件中移出

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,11 +14,118 @@ import { addFeed } from "../../apis";
 import SettingsTabs from "../Settings/SettingsTabs";
 import "./Main.css";
 
+const SettingsModal = ({
+  activeContent,
+  setActiveContent,
+  setVisible,
+  visible,
+}) => (
+  <Modal
+    visible={visible.settings}
+    alignCenter={false}
+    title="Settings"
+    footer={null}
+    unmountOnExit
+    style={{ width: "720px", top: "10%" }}
+    onFocus={() => {
+      if (activeContent) {
+        setActiveContent(null);
+      }
+    }}
+    onCancel={() => setVisible("settings", false)}
+    autoFocus={false}
+    focusLock={true}
+  >
+    <SettingsTabs />
+  </Modal>
+);
+
+const AddFeedModal = ({
+  activeContent,
+  setActiveContent,
+  setVisible,
+  visible,
+}) => {
+  const initData = useStore((state) => state.initData);
+  const groups = useStore((state) => state.groups);
+
+  const [feedModalLoading, setFeedModalLoading] = useState(false);
+  const [feedForm] = Form.useForm();
+
+  const handleAddFeed = async (feed_url, group_id, is_full_text) => {
+    setFeedModalLoading(true);
+    const response = await addFeed(feed_url, group_id, is_full_text);
+    if (response) {
+      await initData();
+      Message.success("Success");
+      setVisible("addFeed", false);
+    }
+    setFeedModalLoading(false);
+    feedForm.resetFields();
+  };
+
+  return (
+    <Modal
+      title="Add Feed"
+      visible={visible.addFeed}
+      unmountOnExit
+      style={{ width: "400px" }}
+      onFocus={() => {
+        if (activeContent) {
+          setActiveContent(null);
+        }
+      }}
+      onOk={feedForm.submit}
+      confirmLoading={feedModalLoading}
+      onCancel={() => {
+        setVisible("addFeed", false);
+        feedForm.resetFields();
+      }}
+    >
+      <Form
+        form={feedForm}
+        layout="vertical"
+        onSubmit={(values) =>
+          handleAddFeed(values.url, values.group, values.crawler)
+        }
+        labelCol={{ span: 7 }}
+        wrapperCol={{ span: 17 }}
+      >
+        <Form.Item label="Feed URL" field="url" rules={[{ required: true }]}>
+          <Input placeholder="Please input feed URL" />
+        </Form.Item>
+        <Form.Item
+          label="Group"
+          required
+          field="group"
+          rules={[{ required: true }]}
+        >
+          <Select placeholder="Please select">
+            {groups.map((group) => (
+              <Select.Option key={group.id} value={group.id}>
+                {group.title}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Fetch original content"
+          initialValue={false}
+          field="crawler"
+          style={{ marginBottom: 0 }}
+          triggerPropName="checked"
+          rules={[{ type: "boolean" }]}
+        >
+          <Switch />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
 const Main = () => {
   const visible = useStore((state) => state.visible);
   const setVisible = useStore((state) => state.setVisible);
-  const initData = useStore((state) => state.initData);
-  const groups = useStore((state) => state.groups);
   const activeContent = useStore((state) => state.activeContent);
   const setActiveContent = useStore((state) => state.setActiveContent);
 
@@ -40,107 +147,21 @@ const Main = () => {
     return () => window.removeEventListener("resize", updateHeight);
   }, []);
 
-  const SettingsModal = () => (
-    <Modal
-      visible={visible.settings}
-      alignCenter={false}
-      title="Settings"
-      footer={null}
-      unmountOnExit
-      style={{ width: "720px", top: "10%" }}
-      onFocus={() => {
-        if (activeContent) {
-          setActiveContent(null);
-        }
-      }}
-      onCancel={() => setVisible("settings", false)}
-      autoFocus={false}
-      focusLock={true}
-    >
-      <SettingsTabs />
-    </Modal>
-  );
-
-  const AddFeedModal = () => {
-    const [feedModalLoading, setFeedModalLoading] = useState(false);
-    const [feedForm] = Form.useForm();
-
-    const handleAddFeed = async (feed_url, group_id, is_full_text) => {
-      setFeedModalLoading(true);
-      const response = await addFeed(feed_url, group_id, is_full_text);
-      if (response) {
-        await initData();
-        Message.success("Success");
-        setVisible("addFeed", false);
-      }
-      setFeedModalLoading(false);
-      feedForm.resetFields();
-    };
-
-    return (
-      <Modal
-        title="Add Feed"
-        visible={visible.addFeed}
-        unmountOnExit
-        style={{ width: "400px" }}
-        onFocus={() => {
-          if (activeContent) {
-            setActiveContent(null);
-          }
-        }}
-        onOk={feedForm.submit}
-        confirmLoading={feedModalLoading}
-        onCancel={() => {
-          setVisible("addFeed", false);
-          feedForm.resetFields();
-        }}
-      >
-        <Form
-          form={feedForm}
-          layout="vertical"
-          onSubmit={(values) =>
-            handleAddFeed(values.url, values.group, values.crawler)
-          }
-          labelCol={{ span: 7 }}
-          wrapperCol={{ span: 17 }}
-        >
-          <Form.Item label="Feed URL" field="url" rules={[{ required: true }]}>
-            <Input placeholder="Please input feed URL" />
-          </Form.Item>
-          <Form.Item
-            label="Group"
-            required
-            field="group"
-            rules={[{ required: true }]}
-          >
-            <Select placeholder="Please select">
-              {groups.map((group) => (
-                <Select.Option key={group.id} value={group.id}>
-                  {group.title}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label="Fetch original content"
-            initialValue={false}
-            field="crawler"
-            style={{ marginBottom: 0 }}
-            triggerPropName="checked"
-            rules={[{ type: "boolean" }]}
-          >
-            <Switch />
-          </Form.Item>
-        </Form>
-      </Modal>
-    );
-  };
-
   return (
     <div className="main" style={{ height: "var(--dynamic-viewport-height)" }}>
       <Outlet />
-      <SettingsModal />
-      <AddFeedModal />
+      <SettingsModal
+        activeContent={activeContent}
+        setActiveContent={setActiveContent}
+        setVisible={setVisible}
+        visible={visible}
+      />
+      <AddFeedModal
+        activeContent={activeContent}
+        setActiveContent={setActiveContent}
+        setVisible={setVisible}
+        visible={visible}
+      />
     </div>
   );
 };
