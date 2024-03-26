a05b5298d25b259d45a8487a939ff9f7f5e488e3
Neko Aria <990879119@qq.com>
2024-03-26 17:47:00 +0800

    fix(vite): 修正输出目录

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
