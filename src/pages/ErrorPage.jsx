5e52d5f328e6dba69aae66381ee51671bbbe3a11
lisicheng <noteperson@gmail.com>
2024-06-06 20:34:25 +0800

    移动端pwa优化

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,16 @@
 import { Button, Result } from "@arco-design/web-react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
+  const error = useRouteError();
+  console.error(error);
 
   return (
     <div id="error-page" style={{ top: "30%", position: "relative" }}>
       <Result
         status="404"
-        subTitle="Whoops, that page is gone. "
+        subTitle={error.statusText || error.message}
         extra={[
           <Button key="back" type="primary" onClick={() => navigate("/")}>
             Back to Home
