4ab373902513be6cb7619cdd108cb69e12b108b0
Neko Aria <990879119@qq.com>
2024-08-31 22:09:00 +0800

    feat(pwa): integrate VitePWA plugin to enhance PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin(), visualizer()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    VitePWA({ registerType: "autoUpdate" }),
+    visualizer(),
+  ],
   server: {
     host: "0.0.0.0",
     port: 3000,
