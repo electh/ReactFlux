51d7fae973f7f2db200ff71cb0c7ba1a3fb6d17d
Neko Aria <990879119@qq.com>
2024-09-05 20:18:56 +0800

    refactor(icon): optimize feed icon retrieval process

--- src/components/ui/FeedIcon.jsx
+++ src/components/ui/FeedIcon.jsx
@@ -1,7 +1,8 @@
-import { getHostname } from "../../utils/url";
+import { getSecondHostname } from "../../utils/url";
 
 const FeedIcon = ({ feed, className = "feed-icon" }) => {
-  const hostname = getHostname(feed.site_url) ?? getHostname(feed.feed_url);
+  const hostname =
+    getSecondHostname(feed.site_url) ?? getSecondHostname(feed.feed_url);
   const iconURL = `https://icons.duckduckgo.com/ip3/${hostname}.ico`;
 
   return <img className={className} src={iconURL} alt="Feed icon" />;
