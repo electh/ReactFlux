82927d4bd0dd95a6f996b7a45740691f6242eb92
Neko Aria <990879119@qq.com>
2024-09-06 10:28:43 +0800

    feat(state): improve createSetter logic for primitive states

--- src/utils/nanostores.js
+++ src/utils/nanostores.js
@@ -1,8 +1,20 @@
-export const createSetter = (store, key) => (updater) => {
-  const state = store.get();
-  if (typeof updater === "function") {
-    store.set({ ...state, [key]: updater(state[key]) });
-  } else {
-    store.set({ ...state, [key]: updater });
-  }
-};
+export const createSetter =
+  (store, key = null) =>
+  (updater) => {
+    const state = store.get();
+
+    if (typeof state === "object" && state !== null) {
+      if (key === null) {
+        return;
+      }
+      if (typeof updater === "function") {
+        store.set({ ...state, [key]: updater(state[key]) });
+      } else {
+        store.set({ ...state, [key]: updater });
+      }
+    } else if (typeof updater === "function") {
+      store.set(updater(state));
+    } else {
+      store.set(updater);
+    }
+  };
