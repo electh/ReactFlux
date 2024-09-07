00d146af8a8e96ce8afffba0fff8e0f9491c0002
Neko Aria <990879119@qq.com>
2024-09-08 00:19:21 +0800

    ci(workflows): schedule weekly job to close stale issues

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    splitVendorChunkPlugin(),
-    VitePWA({ registerType: "autoUpdate" }),
-    visualizer(),
-  ],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" }), visualizer()],
   server: {
     host: "0.0.0.0",
     port: 3000,
   },
-  build: { outDir: "build" },
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
