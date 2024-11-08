5255cd9d3e013e328c48f3e6d34b5b98a84ba96d
Neko Aria <990879119@qq.com>
2024-11-08 21:10:33 +0800

    refactor(parser): extract HTML node handling into separate functions

--- src/components/Article/ArticleDetail.jsx
+++ src/components/Article/ArticleDetail.jsx
@@ -27,78 +27,108 @@ import "./ArticleDetail.css";
 import { useScreenWidth } from "../../hooks/useScreenWidth";
 import "./littlefoot.css";
 
+const handleLinkWithImage = (node, imageSources, togglePhotoSlider) => {
+  const imgNode = node.children[0];
+  if (imgNode.type === "tag" && imgNode.name === "img") {
+    const index = imageSources.findIndex((src) => src === imgNode.attribs.src);
+
+    return (
+      <ImageOverlayButton
+        node={node}
+        index={index}
+        togglePhotoSlider={togglePhotoSlider}
+        isLinkWrapper={true}
+      />
+    );
+  }
+  return node;
+};
+
+const handleBskyVideo = (node) => {
+  const isBskyVideo = /video\.bsky\.app.*thumbnail\.jpg$/.test(
+    node.attribs.src,
+  );
+  if (isBskyVideo) {
+    const thumbnailUrl = node.attribs.src;
+    const playlistUrl = thumbnailUrl.replace("thumbnail.jpg", "playlist.m3u8");
+
+    return (
+      <ReactHlsPlayer
+        src={playlistUrl}
+        controls
+        loop
+        poster={thumbnailUrl}
+        playsInline
+        hlsConfig={{
+          startLevel: -1, // force autolevel
+        }}
+      />
+    );
+  }
+  return node;
+};
+
+const handleImage = (node, imageSources, togglePhotoSlider) => {
+  const bskyVideoPlayer = handleBskyVideo(node);
+  if (bskyVideoPlayer) {
+    return bskyVideoPlayer;
+  }
+
+  const index = imageSources.findIndex((src) => src === node.attribs.src);
+  return (
+    <ImageOverlayButton
+      node={node}
+      index={index}
+      togglePhotoSlider={togglePhotoSlider}
+    />
+  );
+};
+
+const handleCodeBlock = (node) => {
+  // Remove line number text for code blocks in VuePress / VitePress
+  let currentNode = node.next;
+  while (currentNode) {
+    const nextNode = currentNode.next;
+    if (
+      (currentNode.type === "text" &&
+        /^\d+(<br>|\n)*/.test(currentNode.data)) ||
+      (currentNode.type === "tag" && currentNode.name === "br")
+    ) {
+      currentNode.data = "";
+      currentNode.type = "text";
+    }
+    currentNode = nextNode;
+  }
+
+  let codeContent;
+  if (node.children[0]?.name === "code") {
+    const codeNode = node.children[0];
+    codeContent = codeNode.children[0]?.data || "";
+  } else {
+    codeContent = node.children.map((child) => child.data || "").join("");
+  }
+
+  return <CodeBlock>{codeContent}</CodeBlock>;
+};
+
 const getHtmlParserOptions = (imageSources, togglePhotoSlider) => ({
   replace: (node) => {
-    if (node.type === "tag" && node.name === "a" && node.children.length > 0) {
-      const imgNode = node.children[0];
-      if (imgNode.type === "tag" && imgNode.name === "img") {
-        const index = imageSources.findIndex(
-          (src) => src === imgNode.attribs.src,
-        );
-        return (
-          <ImageOverlayButton
-            node={node}
-            index={index}
-            togglePhotoSlider={togglePhotoSlider}
-            isLinkWrapper={true}
-          />
-        );
-      }
-    } else if (node.type === "tag" && node.name === "img") {
-      if (/video\.bsky\.app.*thumbnail\.jpg$/.test(node.attribs.src)) {
-        // bsky video from openrss
-        const thumbnail = node.attribs.src;
-        const videoSrc = thumbnail.replace("thumbnail.jpg", "playlist.m3u8");
-        return (
-          <ReactHlsPlayer
-            src={videoSrc}
-            controls={true}
-            loop={true}
-            poster={thumbnail}
-            playsInline={true}
-            hlsConfig={{
-              startLevel: -1, // force autolevel
-            }}
-          />
-        );
-      }
-
-      const index = imageSources.findIndex((src) => src === node.attribs.src);
-      return (
-        <ImageOverlayButton
-          node={node}
-          index={index}
-          togglePhotoSlider={togglePhotoSlider}
-        />
-      );
-    } else if (node.type === "tag" && node.name === "pre") {
-      // Remove line number text for code blocks in VuePress / VitePress
-      let currentNode = node.next;
-      while (currentNode) {
-        const nextNode = currentNode.next;
-        if (
-          (currentNode.type === "text" &&
-            /^\d+(<br>|\n)*/.test(currentNode.data)) ||
-          (currentNode.type === "tag" && currentNode.name === "br")
-        ) {
-          currentNode.data = "";
-          currentNode.type = "text";
-        }
-        currentNode = nextNode;
-      }
-
-      let codeContent;
-      if (node.children[0]?.name === "code") {
-        const codeNode = node.children[0];
-        codeContent = codeNode.children[0]?.data || "";
-      } else {
-        codeContent = node.children.map((child) => child.data || "").join("");
-      }
-
-      return <CodeBlock>{codeContent}</CodeBlock>;
+    if (node.type !== "tag") {
+      return node;
     }
 
-    return node;
+    switch (node.name) {
+      case "a":
+        return node.children.length > 0
+          ? handleLinkWithImage(node, imageSources, togglePhotoSlider)
+          : node;
+      case "img":
+        return handleImage(node, imageSources, togglePhotoSlider);
+      case "pre":
+        return handleCodeBlock(node);
+      default:
+        return node;
+    }
   },
 });
 
