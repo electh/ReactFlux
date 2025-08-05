3e53cde9d0901f3060acba8b2deb7da75942fb8d
NekoAria <NekoAria@users.noreply.github.com>
2025-08-06 01:15:13 +0800

    fix(auth): remove docker URL validation for such a frontend-only project

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,8 @@
 import isURL from "validator/lib/isURL"
 
-const isValidDockerURL = (url) => {
-  try {
-    const parsedUrl = new URL(url)
-    const { hostname, port } = parsedUrl
-    return !!hostname && !!port
-  } catch {
-    return false
-  }
-}
-
 const isValidAuth = (auth) => {
   const { server, token, username, password } = auth
-  if (!server || (!isURL(server, { require_protocol: true }) && !isValidDockerURL(server))) {
+  if (!server || !isURL(server, { require_protocol: true })) {
     return false
   }
   return token || (username && password)
